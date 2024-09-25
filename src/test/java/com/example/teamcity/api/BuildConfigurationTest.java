430988866ecdd6601f56122c502b6356465f28d4
Sergey Terentev <sergey.terentev@lamoda.ru>
2024-09-26 01:16:46 +0300

    first improvement: auth Spec & properties

--- src/test/java/com/example/teamcity/api/BuildConfigurationTest.java
+++ src/test/java/com/example/teamcity/api/BuildConfigurationTest.java
@@ -1,5 +1,7 @@
 package com.example.teamcity.api;
 
+import com.example.teamcity.api.models.User;
+import com.example.teamcity.api.spec.Specifications;
 import io.restassured.RestAssured;
 import org.apache.http.HttpStatus;
 import org.testng.annotations.Test;
@@ -8,9 +10,17 @@ public class BuildConfigurationTest extends BaseApiTest{
 
     @Test
     public void buildConfigurationTest() {
-        var token = RestAssured.get("http://admin:admin@192.168.0.103:8111/authenticationTest.html?csrf")
-                .then().assertThat().statusCode(HttpStatus.SC_OK).
-                extract().asString();
+        var user = User.builder()
+                .username("admin")
+                .password("admin")
+                .build();
+
+        var token = RestAssured
+                .given()
+                .spec(Specifications.getSpec().authSpec(user))
+                .get("/authenticationTest.html?csrf")
+                .then().assertThat().statusCode(HttpStatus.SC_OK)
+                .extract().asString();
 
         System.out.println(token);
     }
