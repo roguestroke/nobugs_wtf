d0bf943b79bdced26b9f0de146328a7c4919324d
Sergey Terentev <sergey.terentev@lamoda.ru>
2024-09-27 00:59:21 +0300

    checked & unchecked base classes

--- src/main/java/com/example/teamcity/api/requests/CrudInterface.java
+++ src/main/java/com/example/teamcity/api/requests/CrudInterface.java
@@ -5,7 +5,7 @@ import com.example.teamcity.api.models.BaseModel;
 public interface CrudInterface {
 
     Object create(BaseModel model);
-    Object read(String Id);
-    Object update(String Id, BaseModel model);
-    Object delete(String Id);
+    Object read(String id);
+    Object update(String id, BaseModel model);
+    Object delete(String id);
 }
